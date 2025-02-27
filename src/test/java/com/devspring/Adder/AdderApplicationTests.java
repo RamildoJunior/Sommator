6c655dab73e270dfaf2a0286235b2cde753817b2
RamildoJunior <rpsjuninho@gmail.com>
2025-02-27 20:40:48 -0300

    Inicio do projeto

---
+++ src/test/java/com/devspring/Adder/AdderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devspring.Adder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
